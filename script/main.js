5b5332bdefdbcddd4fde48ff0505e2f44128ad52
amirbasiri2000 <78272810+amirbasiri2000@users.noreply.github.com>
2022-01-19 19:39:15 +0330

    second

--- script/main.js
+++ script/main.js
@@ -1,34 +1,53 @@
-(function() {
+$(document).ready(function(){
+	$(".navbar-togle-button").click(function() {
+		$(".navbar-nav").fadeToggle().toggleClass("d-block")
+	})
+})
 
-  var siteSticky = function() {
-		$(".js-sticky-header").sticky({topSpacing:0});
-	};
-	siteSticky();
 
-	var siteDropdown = function() {
 
-		$('nav .dropdown').hover(function(){
-			var $this = $(this);
-			$this.addClass('show');
-			$this.find('> a').attr('aria-expanded', true);
-			$this.find('.dropdown-menu').addClass('show');
-		}, function(){
-			var $this = $(this);
-				$this.removeClass('show');
-				$this.find('> a').attr('aria-expanded', false);
-				$this.find('.dropdown-menu').removeClass('show');
+
+
+
+
+
+
+
+
+
+
+
+// (function() {
+
+//   var siteSticky = function() {
+// 		$(".js-sticky-header").sticky({topSpacing:0});
+// 	};
+// 	siteSticky();
+
+// 	var siteDropdown = function() {
+
+// 		$('nav .dropdown').hover(function(){
+// 			var $this = $(this);
+// 			$this.addClass('show');
+// 			$this.find('> a').attr('aria-expanded', true);
+// 			$this.find('.dropdown-menu').addClass('show');
+// 		}, function(){
+// 			var $this = $(this);
+// 				$this.removeClass('show');
+// 				$this.find('> a').attr('aria-expanded', false);
+// 				$this.find('.dropdown-menu').removeClass('show');
                 
-		});
+// 		});
 
 
-		$('#dropdown04').on('show.bs.dropdown', function () {
-		  console.log('show');
-		});
+// 		$('#dropdown04').on('show.bs.dropdown', function () {
+// 		  console.log('show');
+// 		});
 
-	  $('.navbar .dropdown > a').click(function(){
-	    location.href = this.href;
-	  });
-	}; 
-	siteDropdown();
+// 	  $('.navbar .dropdown > a').click(function(){
+// 	    location.href = this.href;
+// 	  });
+// 	}; 
+// 	siteDropdown();
 
-});
+// });
